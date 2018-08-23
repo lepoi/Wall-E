e0c0ece689e09e35127030997fad44082d8086f4
lepoi <terminallyquack@gmail.com>
2018-08-23 00:35:41 -0700

    Initial commit

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-int main() {
-	printf("%s", "1. Insert magic number\n");
-	printf("%s", "2. Everything else :)\n");
+int main(int n, char **args) {
+	FILE *source;
+	char *line = NULL, *result;
+	size_t len = 0;
+	ssize_t read;
+
+	if (! args[1]) {
+		printf("Needs more arguments!\n");
+		return 1;
+	}
+
+	source = fopen(args[1], "r");
+	
+	if (!source) {
+		printf("Error opening %s\n", args[1]);
+		return 1;
+	}
+
+	result = "";
+
+	printf("Printing input file:\n");
+	printf("-----\n");
+	while (read = getline(&line, &len, source) != -1) {
+        printf("Retrieved: %s", line);
+	}
+
 	return 0;
 }
