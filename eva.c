bccf342df81c5916f2813cb816a997dfbd3f01b4
Edgar Magdaleno <edgarmv97@gmail.com>
2018-09-03 16:19:41 -0700

    Update

--- eva.c
+++ eva.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
 
-int main() {
+#define MAX_STACK_SIZE 128
 
+struct stack_item {
+   char type;
+   union {
+       int i;
+       double d;
+       char c;
+       char *s;
+   } content;
+} stack[MAX_STACK_SIZE];
+
+void run(FILE *fp) {
+	char c;
+	int size = 0;
+	fseek(fp, 20, SEEK_SET);
+	do {
+		c = fgetc(fp);
+		printf("c = %x\n", c);
+		switch (c) {
+			case '\x06': {
+				struct stack_item i = {};
+				i.type = 1;
+				printf("r: %i\n", fscanf(fp, "%c", &i.content.i));
+				stack[size++] = i;
+			} break;
+		}
+
+	} while (c != EOF);
+		printf("Stack contents:\n");
+		for (int i = 0; i < size; i++) {
+			printf("%i\n", stack[i].content.i);
+		}
 }
+
+int main (int argc, char **argv){
+    FILE *fp = fopen("out.bin", "rb+");
+
+    run(fp);
+	return 0;
+}
+
